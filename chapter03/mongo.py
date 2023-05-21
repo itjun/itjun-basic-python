9d5ae99d42da9df0100d2792f77ffd65c724927e
HUANG Rongjun <l1091462907@gmail.com>
2023-05-22 01:17:09 +0800

    add mongo demo

---
+++ chapter03/mongo.py
@@ -0,0 +1,16 @@
+import pymongo
+from pymongo import MongoClient
+uri = 'mongodb://127.0.0.1:27017'
+client = MongoClient(uri,
+                    username='root',
+                    password='root',
+                    authMechanism='SCRAM-SHA-256')
+
+db = client["eshop"]
+user_coll = db["users"]
+new_user = {"username":"nina", "password":"xxx", "email":"123456@qq.com"  }
+# result = user_coll.insert_one(new_user)
+# result
+
+result = user_coll.update_one({"username":"nina"}, {"$set":{"phone":"13927470636"}})
+print(result)
